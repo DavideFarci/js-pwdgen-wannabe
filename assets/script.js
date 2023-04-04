5b639c60f32371636b5d4e620840ab33c717a116
DavideFarci <yank92.df@gmail.com>
2023-04-04 15:45:14 +0200

    add exercise

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
-alert("ciao")
+/* 
+    chiedere il nome all'utente
+    chiedere il cognome all'utente
+    chiedere il suo colore preferito all'utente
+    scrivere sulla pagina il nome generato 
+*/
+
+const userName = prompt("Qual'è il tuo nome?");
+
+const userLastname = prompt("Qual'è il tuo cognome?")
+
+const userColor = prompt("Qual'è il tuo colore preferito?")
+
+const eleSpan = document.getElementById("pwd")
+eleSpan.innerHTML = userName + userLastname + userColor + 23
